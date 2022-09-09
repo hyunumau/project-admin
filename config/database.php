068df1e1dcfd3b97aaf52820ecbbc4cd77e8e393
Luu Thanh Tan <hyunumau@gmail.com>
2022-09-09 09:40:57 +0700

    pgsql

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('postgres://qndsxidwpehhdy:9ae7f8a6f457223be00bb88751109ef36d863fcac9bf404291ec2b518f071d29@ec2-35-168-122-84.compute-1.amazonaws.com:5432/d5291vb9puu2sa');
+
 return [
 
     /*
@@ -65,12 +67,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => $DATABASE_URL['url'],
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'], '/'),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
