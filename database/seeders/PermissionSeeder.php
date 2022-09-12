85e26df666ea16cb29c722bf927164714fe82cf5
Luu Thanh Tan <hyunumau@gmail.com>
2022-09-12 09:19:16 +0700

    heroku

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -6,60 +6,67 @@ use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\PermissionRegistrar;
-use \App\Models\User;
 
 class PermissionSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Create the initial roles and permissions.
      *
      * @return void
      */
     public function run()
     {
         // Reset cached roles and permissions
-
-        Permission::insert([
-            [
-                'name' => 'permission read',
-                'guard_name' => 'web'
-            ],
-            [
-                'name' => 'role read',
-                'guard_name' => 'web'
-            ],
-            [
-                'name' => 'role create',
-                'guard_name' => 'web'
-            ],
-            [
-                'name' => 'role edit',
-                'guard_name' => 'web'
-            ],
-            [
-                'name' => 'role delete',
-                'guard_name' => 'web'
-            ],
-            [
-                'name' => 'user read',
-                'guard_name' => 'web'
-            ],
-            [
-                'name' => 'user create',
-                'guard_name' => 'web'
-            ],
-            [
-                'name' => 'user edit',
-                'guard_name' => 'web'
-            ],
-            [
-                'name' => 'user delete',
-                'guard_name' => 'web'
-            ],
-            [
-                'name' => 'articles publish',
-                'guard_name' => 'web'
-            ],
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+        // create permissions
+        $permissions = [
+            'permission read',
+            'role read',
+            'role create',
+            'role edit',
+            'role delete',
+            'user read',
+            'user create',
+            'user edit',
+            'user delete',
+            'articles publish',
+        ];
+        foreach ($permissions as $permission) {
+            Permission::create([
+                'name' => $permission,
+                'guard_name' => 'web',
+            ]);
+        }
+        // create roles and assign existing permissions
+        $role1 = Role::create(['name' => 'writer']);
+        $role1->givePermissionTo('permission read');
+        $role1->givePermissionTo('role read');
+        $role1->givePermissionTo('user read');
+        $role2 = Role::create(['name' => 'admin']);
+        foreach ($permissions as $permission) {
+            $role2->givePermissionTo($permission);
+        }
+        $role3 = Role::create(['name' => 'super-admin']);
+        // gets all permissions via Gate::before rule; see AuthServiceProvider
+        // create demo users
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@example.com',
+            'password' => bcrypt('12345678'),
+            'is_superadmin' => true
         ]);
+        $user->assignRole($role3);
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('12345678'),
+        ]);
+        $user->assignRole($role2);
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Writer',
+            'email' => 'test@example.com',
+            'password' => bcrypt('12345678'),
+        ]);
+        $user->assignRole($role1);
     }
 }
